17aeab22bef815436f03df35bcfc6170613330fc
Anish-1144 <dev.anishmourya27@gmail.com>
2024-09-07 22:02:33 +0530

    .env,auth

--- mega-blog-app/src/App.jsx
+++ mega-blog-app/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function App() {
+  console.log("hello");
+  
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
   return (
     <div>
-      
+      <h1>hellow blog</h1>
     </div>
   )
 }
