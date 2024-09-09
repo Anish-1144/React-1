b70c4021de9125bd6eb72830c14ceae7b1d10fd1
Anish-1144 <dev.anishmourya27@gmail.com>
2024-09-09 21:28:31 +0530

    Updated .gitignore and added new files

--- mega-blog-app/src/App.jsx
+++ mega-blog-app/src/App.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+import  authService from './appwrite/auth'
+import {login ,logout} from './store/authSlice'
+import { Footer } from './components'
+import { Header } from './components'
 
 function App() {
-  console.log("hello");
-  
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
-  return (
-    <div>
-      <h1>hellow blog</h1>
-    </div>
-  )
+  const [loading ,setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(()=>{
+authService.getcurrentUser()
+.then((userData)=>{
+  if (userData) {
+    dispatch(login({userData}))
+    
+  } else {
+    dispatch(logout())
+    
+  }
+})
+
+.finally(()=>setLoading(false))
+
+  },[])
+   
+  return !loading ? (<div className='min-h-screen flex flex-wrap  bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        {/* outlet */}
+      </main>
+      <Footer/>
+      
+      </div> 
+  </div>) : null;
+
 }
 
 export default App
